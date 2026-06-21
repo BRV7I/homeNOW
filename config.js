cae43738b4801c8a4f5867be4ade5ebe8d00ce0a
BRV7I <113093854+BRV7I@users.noreply.github.com>
2026-06-21 14:58:20 +0200

    config.js

--- config.js
+++ config.js
@@ -1,23 +1,12 @@
 window.CONFIG = {
-
   SUPABASE_URL: "https://lzmdzyfurebdcudtkywp.supabase.co",
-
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6bWR6eWZ1cmViZGN1ZHRreXdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwMzI2ODksImV4cCI6MjA5NzYwODY4OX0.P1cjW-kiwSOTOTZ1Kub_9v2qtHWiwrXLmzluKWQNRsQ",
-
   BUCKET: "casa-files",
-
   PROPERTY_ID: "11111111-1111-1111-1111-111111111111",
-
   PASSWORD: "XY*362941",
-
   PIN: "2580",
-
   OWNER_NAME: "Simone",
-
   AGENT_NAME: "Simone Marchegiani",
-
   AGENT_PHONE: "+39 334 370 0189",
-
   AGENCY: "Professione Casa"
-
 };
